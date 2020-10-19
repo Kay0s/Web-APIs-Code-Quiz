3632d54d743d5fd992d56a8761aaf5f68351e060
Kay0s <hamilton.kristina@gmail.com>
2020-10-18 19:51:13 -0500

    Added pseudo code.

---
+++ scripts.js
@@ -0,0 +1,42 @@
+//Coding Quiz
+
+
+
+//Write coding questions for quiz
+
+// Constant/Persistant Elements
+//    a. "On Screen Timer" - displays "Time Left" function of ("Questions Remaining and "your final score") in upper right hand corner (purple font)
+//    b. "Highscores" link - upper left hand corner (purple font) function of (<ol>"your final score"</ol>
+//    c. "your final score"- tallied on console and shown on "Final Score Page" and on "Highscores" listed as <tr?> with purple background highlight with onclick
+//    d. "Count of Number of Questions" vs "Questions Remaining" function of ("Wrong" = "Time Left" - 10 seconds")
+//    e. "Time Left" in upper right hand corner (purple font) displayed by "On Screen Timer" function of ("Wrong" = "Time Left" && "On Screen Timer" - 10 seconds")
+// 
+// 1. Home Screen:
+//    a. <h1>"Coding Quiz Challenge"</h1> with "Instructions" <p>Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p>
+//    b. <on click> "Start Quiz"<submit> button (bg purple with white font/hover CSS styling) which starts <event listener> "Time Left' &&  "On Screen Timer"
+//    c. starts "On Screen Timer" - which stays in upper right hand corner and displays "Time Left"  (purple font)
+//    d. "Highscores" link - upper left hand corner (purple font)
+// 2. Multiple Choice Question Screens:
+//    a. "On Screen Timer" - upper right hand corner (purple font)
+//    b."Submit" button for choice, (bg purple with white font/hover CSS styling)  -  when pressed
+//   b1. if <on click> "Right" - ("your final score" && + 1 ) + <event listener> text w/<hr>"Correct" message + next question appears
+//   b2. if <on click> "Wrong" - ("Time Left" && "On Screen Timer" - 10 seconds) <event listener> text w/<hr>"Wrong" message + next question appears
+//    c. "Highscores" link - upper left hand corner (purple font)
+// 3. Quiz Ends:
+//    a1. if all questions answered ("Questions Remaining" === "0")
+//    a2. else if time turns to 0 on timer ("Time Left" ==== "0")
+//    b. When quiz ends
+// 4. Final Score Page:
+//    a. "your final score" - shown on page, push to 'Highscores" array and display as <ul></ul> in <tr>
+//    b. <event listener> form box to enter "Initials" - ("Initials" + "your final score" push to "HighScores Page")
+//    c. <on click> "Submit" button - links to High Scores (bg purple with white font/hover CSS styling)
+//    d. <on click>"Go Back" button (bg purple with white font/hover CSS styling) - links to Home Screeen
+//    e. "On Screen Timer" - upper right hand corner (purple font)
+// 5. Highscores Page:
+//    a. High Scores listed as <tr> with purple background highlight with onclick
+//    a. "Go Back" button - links to Home Screen (bg purple with white font/hover CSS styling)
+//    b. "Clear High Scores" button (bg purple with white font/hover CSS styling)
+
+
+
+
