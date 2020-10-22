e99f2294b774ec96f4b9332d044f004620a8d54f
Kay0s <hamilton.kristina@gmail.com>
2020-10-21 19:45:58 -0500

    upate even listener

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 //Coding Quiz
 
-
-
 //Write coding questions for quiz
 
 // Constant/Persistent Elements
@@ -10,7 +8,7 @@
 //    c. "your final score"- tallied on console and shown on "Final Score Page" and on "Highscores" listed as <tr?> with purple background highlight with onclick
 //    d. "Total Questions" vs "Questions Remaining" function of ("Wrong" = "Time Left" - 10 seconds")
 //    e. "Seconds Left" in upper right hand corner (purple font) displayed by "On Screen Timer" function of ("Wrong" = "Seconds Left" && "On Screen Timer" - 10 seconds")
-// 
+//
 // 1. Instructions Message:
 //    a. <h1>"Coding Quiz Challenge"</h1> with "Instructions" <p>Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p>
 //    b. <on click> "Start Quiz"<submit> button (bg purple with white font/hover CSS styling) which starts <event listener> "Time Left' &&  "On Screen Timer"
@@ -37,157 +35,47 @@
 //    a. "Go Back" button - links to Home Screen (bg purple with white font/hover CSS styling)
 //    b. "Clear High Scores" button (bg purple with white font/hover CSS styling)
 
+// let totalSeconds = 75;
 
+// // //  changes the ul id = "seconds"
+// const secondsDisplay = document.getElementbyID("seconds");
 
+// setInterval(startCountdown, 1000);
 
+// //start quiz button function to bring question text in and start timer
+// function startCountdown() {
+//   let seconds = totalSeconds;
 
-// //  changes the div id = "seconds"
-// let secondsDisplay = document.querySelector("#seconds");
+//   document.getElementbyID("seconds").innerHtml = "seconds";
 
-// let totalSeconds = 0;
-// let status = "Time";
-// let interval;
+//   time--;
+// }
+// document.getElementbyId("startQuiz").addEventListener("click", removeIntro);
+// function removeIntro() {
+//   document.getElementById("text-center text-black").style.display = "none";
+// }
 
-// // formats the "On Screen Timer" to have two digits
-// function getFormattedSeconds() {
-//    let secondsLeft = (totalSeconds - secondsElapsed) % 60;
-  
-//     var formattedSeconds;
-  
-//     if (secondsLeft < 10) {
-//       formattedSeconds = "0" + secondsLeft;
-//     } else {
-//       formattedSeconds = secondsLeft;
-//     }
-  
-//     return formattedSeconds;
-//   }
+// document.getElementById("startQuiz").addEventListener("click", flyinQuest1);
+// function flyinQuest1() {
+//   document.getElementById("questions-text1").style.display = "visible";
+// }
 
 //   /* This function retrieves the values from the html input elements and resets "On Screen Timer and Time Left*/
-// function setTime() {
-//     let secondsLeft;
-  
-//     if (status === "Correct") {
-//       secondsLeft = secondsLeftInput.value.trim();
-//     } else (status === "Wrong"); {
-//       secondsLeft = secondsLeftInput.value.trim() - 10 seconds;
-//     }
-  
-//     clearInterval(interval);
-//     totalSeconds = minutes * 60;
-//   }
+//   function updateTime(currentTime) {
+//     return currentTime - 10
+//      }
 
-//    // check to see if time has run out
-//    if (questionsRemaining >= totalQuestions) {
-//     continueTimer();
-// }
-   
-//    if (questionsRemaining === 0) {
-//     alert("Quiz is over");
-//     stopTimer();
-// }
-    
-// if (secondsRemaining === 0) {
-//     alert("Quiz is over");
-//     stopTimer();
-// }
-   
+//       // Set "On Screen Timer" fuctions
+//      function startTimer() {
 
-// // Set "On Screen Timer" fuctions
-// function startTimer() {
-//   setTime();
+//          /* The "interval" variable here using "setInterval()" begins the recurring increment of the
+//             secondsElapsed variable which is used to check if the time is up */
+//            setInterval(function() {
+//            totalSeconds --
 
-//   if (totalSeconds > 0) {
-//     /* The "interval" variable here using "setInterval()" begins the recurring increment of the
-//        secondsElapsed variable which is used to check if the time is up */
-//       interval = setInterval(function() {
-//         secondsElapsed++;
+//              //  renderTime() is called here once every second.
+//              renderTime();
+//            }, 1000);
+//        }
 
-//         //  renderTime() is called here once every second.
-//         renderTime();
-//       }, 1000);
-//   } 
-// }
-
-// /* This function stops the setInterval() set in startTimer but does not
-//    reset the secondsElapsed variable and does not reset the time by calling "setTime()" */
-//    function pauseTimer() {
-//     clearInterval(interval);
-//     renderTime();
-//   }
-  
-//   /* This function stops the interval and also resets secondsElapsed
-//      and calls "setTime()" which effectively reset the timer
-//      to the input selections workMinutesInput.value and restMinutesInput.value */
-//   function stopTimer() {
-//     secondsElapsed = 0;
-//     setTime();
-//     renderTime();
-//   }
-
-//    //setTime and renderTime 
-//    renderTime();
-//  }
- 
-//  function setSecondsLeft() {
-//    localStorage.setItem(
-//      "timer",
-//      JSON.stringify({
-//        secondsLeft: secondsLeftInput.value.trim()
-//      })
-//    );
-//  }
- 
-//  submitButton.addEventListener("click", startTimer);
-//  wrongButton.addEventListener("click", reduceTimer);
-//  rightButton.addEventListener("click", addScore);
-//  initialsButton.addEventListener("click", addHighscoresArray);
-//  gobackButton.addEventListener("click", gobackInstructions);
-//  clearHighScores.addEventListener("click", deleteHighscoresArray);
-//  highScoresButton.addEventListener("click", displayHighscoresArray);
- 
-// get right or wrong message with on click
-document.getElementById("r1Btn").addEventListener("click", function() {
-  document.getElementById("r1").innerHTML = "Right";
-});
-
-document.getElementById("w1Btn").addEventListener("click", function() {
-  document.getElementById("w1").innerHTML = "Wrong";
-});
-document.getElementById("w2Btn").addEventListener("click", function() {
-  document.getElementById("w2").innerHTML = "Wrong";
-});
-document.getElementById("w3Btn").addEventListener("click", function() {
-  document.getElementById("w3").innerHTML = "Wrong";
-});
-document.getElementById("r2Btn").addEventListener("click", function() {
-  document.getElementById("r2").innerHTML = "Right";
-});
-
-document.getElementById("w4Btn").addEventListener("click", function() {
-  document.getElementById("w4").innerHTML = "Wrong";
-});
-document.getElementById("w5Btn").addEventListener("click", function() {
-  document.getElementById("w5").innerHTML = "Wrong";
-});
-document.getElementById("w6Btn").addEventListener("click", function() {
-  document.getElementById("w6").innerHTML = "Wrong";
-});
-
-
-// form questionsAvailable array
-let questionsAvailable = [
-  {"question" : "What is the DOM API?" , "options" : ["Data representation of the objects that comprise the structure and content of a document on the web","Doing Outstanding Math","Domestic Over Minutes","Dividing Object Miniscule"], "answer" : "Data representation of the objects that comprise the structure and content of a document on the web"},
-  {"question" : "What is JQeury?" , "options" : ["A JavaScript Library","Just a question","Elements of the letter j","Justice without question"], "answer" :"A JavaScript Library"},
-]
-
-
-// randomize questionsAvailable and splice into questionsUsed array
-let questionsUsed = questionsAvailable.splice(Math.floor(Math.random()*questionsAvailable.length),1)[i];
-
-
-// display question from array
-function display_question()
-{
-   let question = document.getElementById("questions"(text))
-  
+// /
